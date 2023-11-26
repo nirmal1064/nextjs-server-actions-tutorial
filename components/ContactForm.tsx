048900fb9ba4822387282f55bdbae7d6289d670e
Nirmal Kumar <nirmal1064@gmail.com>
2023-11-26 18:15:09 +0530

    created server action to add a new contact

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,6 +1,21 @@
+import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
 export default function ContactForm() {
+  async function addContact(formData: FormData) {
+    "use server";
+    await prisma.contact.create({
+      data: {
+        name: formData.get("name") as string,
+        phone: formData.get("phone") as string,
+        city: formData.get("city") as string
+      }
+    });
+    revalidatePath("/");
+  }
+
   return (
-    <form className="flex flex-col gap-2">
+    <form className="flex flex-col gap-2" action={addContact}>
       <input
         type="text"
         name="name"
