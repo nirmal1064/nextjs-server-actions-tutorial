24fb53455b58fc4aa2ada209ca8b4384b11af3be
Nirmal Kumar <nirmal1064@gmail.com>
2023-11-26 21:36:18 +0530

    created server action to edit a contact

--- app/[id]/view/page.tsx
+++ app/[id]/view/page.tsx
@@ -1,4 +1,5 @@
 import ContactCard from "@/components/ContactCard";
+import ContactForm from "@/components/ContactForm";
 import prisma from "@/lib/db";
 
 type Props = { params: { id: string } };
@@ -12,6 +13,7 @@ export default async function EditContact({ params }: Props) {
 
   return (
     <main className="flex flex-col min-h-screen justify-center items-center bg-gray-50">
+      <ContactForm contact={contact} />
       <h1 className="font-semibold">Single Contact</h1>
       {contact ? (
         <ContactCard key={contact.id} contact={contact} />
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,7 +1,10 @@
 import prisma from "@/lib/db";
+import { Contact } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
-export default function ContactForm() {
+type Props = { contact?: Contact | null };
+
+export default function ContactForm({ contact }: Props) {
   async function addContact(formData: FormData) {
     "use server";
     await prisma.contact.create({
@@ -14,28 +17,46 @@ export default function ContactForm() {
     revalidatePath("/");
   }
 
+  async function updateContact(formData: FormData) {
+    "use server";
+    await prisma.contact.update({
+      where: { id: contact?.id },
+      data: {
+        name: formData.get("name") as string,
+        phone: formData.get("phone") as string,
+        city: formData.get("city") as string
+      }
+    });
+    revalidatePath(`/${contact?.id}/view`);
+  }
+
   return (
-    <form className="flex flex-col gap-2" action={addContact}>
+    <form
+      className="flex flex-col gap-2"
+      action={contact ? updateContact : addContact}>
       <input
         type="text"
         name="name"
         placeholder="Contact Name"
         className="p-2 border border-black"
+        defaultValue={contact?.name}
       />
       <input
         type="text"
         name="phone"
         placeholder="Contact Phone Number"
         className="p-2 border border-black"
+        defaultValue={contact?.phone}
       />
       <input
         type="text"
         name="city"
         placeholder="Enter City"
         className="p-2 border border-black"
+        defaultValue={contact?.city}
       />
       <button type="submit" className="bg-blue-500 text-white rounded-md p-1">
-        Add Contact
+        {contact ? "Update Contact" : "Add Contact"}
       </button>
     </form>
   );
